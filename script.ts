5f10c28c86288a8c602fdf901b6e20fe206238c3
Солосин Макар <itami@stud.vvsu.ru>
2024-01-31 17:50:38 +1000

    point

---
+++ script.ts
@@ -0,0 +1,18 @@
+class ClassName{}
+class Point{x: number; y: number;}
+let alex: Point = new Point();
+function gpoint() {
+    gpoint.x = 1;
+    gpoint.y = 2;
+}
+gpoint()
+console.log(alex)
+
+class Point1{width: number; height: number;}
+let alex1: Point1 = new Point1();
+function gpoint1() {
+    gpoint1.width = 1;
+    gpoint1.height = 2;
+}
+gpoint1()
+console.log(alex1)
